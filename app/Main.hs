1e5d10a24d7b097615a8a1b45124677f50825b8b
ncihnegn <ncihnegn@users.noreply.github.com>
2020-04-10 14:11:19 -0700

    Add Lexer & Parser based on Parsec

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,21 @@
 module Main where
 
-import Lib
+import Control.Monad.Trans
+import Parser
+import System.Console.Haskeline
+
+process :: String -> IO ()
+process line = do
+  let res = parseTopLevel line
+  case res of
+    Left err -> print err
+    Right ex -> mapM_ print ex
 
 main :: IO ()
-main = someFunc
+main = runInputT defaultSettings loop
+  where
+    loop = do
+      minput <- getInputLine "ready> "
+      case minput of
+        Nothing -> outputStrLn "Goodbye."
+        Just input -> (liftIO $ process input) >> loop
